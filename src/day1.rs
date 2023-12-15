685e61a4d554a69856f69c4baed23e95ef90f2f6
Derek Santos <dersanto@cisco.com>
2023-12-14 23:05:58 -0500

    Checkpoint d1

--- src/day1.rs
+++ src/day1.rs
@@ -1,18 +1,66 @@
+use indexmap::{indexmap, IndexMap};
 use std::fs::read_to_string;
 use std::path::Path;
 
+fn create_word_to_num_mapping() -> IndexMap<String, String> {
+    return indexmap! {
+        "one".to_owned() => "1".to_owned(),
+        "two".to_owned() => "2".to_owned(),
+        "three".to_owned() => "3".to_owned(),
+        "four".to_owned() => "4".to_owned(),
+        "five".to_owned() => "5".to_owned(),
+        "six".to_owned() => "6".to_owned(),
+        "seven".to_owned() => "7".to_owned(),
+        "eight".to_owned() => "8".to_owned(),
+        "nine".to_owned() => "9".to_owned(),
+    };
+}
+
 pub fn run() {
-    let mut lines: Vec<&str> = vec![];
+    // Read file
+    let mut lines: Vec<String> = vec![];
     let file_path = Path::new("./src/assets/day1/part_one.txt");
     let file_content = read_to_string(file_path).unwrap().to_string();
+
+    // Create word to mapping
+    let map = create_word_to_num_mapping();
+
+    // Iterate and change all occurences of word -> number
     for line in file_content.lines() {
+        print!("Line: {}, ", line);
+        let mut line = line.to_owned();
+
+        let mut index_map: IndexMap<usize, String> = IndexMap::new();
+
+        for (word, _) in &map {
+            match line.find(word) {
+                Some(position) => {
+                    index_map.insert(position, word.to_owned());
+                }
+                None => {}
+            }
+        }
+
+        let mut keys: Vec<usize> = index_map.keys().cloned().collect();
+
+        keys.sort();
+
+        // Sort by position and replace all occurences...
+        // This won't match something that was partially replaced before
+        for position in keys.iter(){
+
+            let value = index_map.get(position).unwrap();
+            line = line.replace(value, map.get(value).unwrap());
+        }
+
+        println!(" -> {}", line);
         lines.push(line);
     }
 
     let mut numbers_to_sum: Vec<String> = vec![];
 
+    // Go through each line and actually try to find the two numbers needed
     for word in lines.iter() {
-        let word = word.to_string();
         let mut first_number: Option<char> = None;
         let mut second_number: Option<char> = None;
 
@@ -34,17 +82,14 @@ pub fn run() {
             println!("Did not find numbers for '{}'", word);
             continue;
         }
-        println!(
-            "Adding to sum: {}{}",
-            first_number.unwrap(),
-            second_number.unwrap()
-        );
+        // println!( "Adding to sum: {}{}", first_number.unwrap(), second_number.unwrap());
         numbers_to_sum
             .push(format!("{}{}", first_number.unwrap(), second_number.unwrap()).to_string());
     }
 
     let mut final_sum: u32 = 0;
 
+    // Sum them all up
     for number in numbers_to_sum {
         let number = number.parse::<u32>().unwrap();
         final_sum += number;
